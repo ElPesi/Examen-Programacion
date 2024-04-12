d855080de5956a30ac3de8fe7acb37efdc3bb0f2
Tito Calderon <162237728+ElPesi@users.noreply.github.com>
2024-04-12 11:31:24 -0300

    Update ej2.py

--- ej2.py
+++ ej2.py
@@ -1,1 +1,24 @@
+  
+def crearsublistas():
+    num_sublistas = int(input("Ingrese la cantidad de sublistas que desea crear: "))
+    main_list = []
+    for i in range(num_sublistas):
+        sublista = []
+        while True:
+            num = int(input("Ingrese un número para la sublistas (ingrese -1 para terminar): "))
+            if num == -1:
+                break
+            sublista.append(num)
+        main_list.append(sublista)
+    return main_list
+
+###  ##   ## ##             ## ##   ### ###            ### ##   ### ##   ## ##    ### ###  ### ###  
+# ## ##  ##   ##           ##   ##   ##  ##            ##  ##   ##  ##  ##   ##   ##  ##   ##  ##  
+## # #  ##   ##           ####      ##                 ##  ##   ##  ##  ##   ##   ##       ##      
+##  ##   ##   ##            #####    ## ##             ##  ##   ## ##   ##   ##   ## ##    ## ##   
+##   ##  ##   ##               ###   ##                ## ##    ## ##   ##   ##   ##       ##      
+##   ##  ##   ##           ##   ##   ##  ##            ##       ##  ##  ##   ##   ##       ##  ##  
+##   ##   ## ##             ## ##   ### ###           ####     #### ##   ## ##   ####      ### ###  
+                                                                                            
+
 
