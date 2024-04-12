5c2bb5096e4d7ef7e882e0d402f7546abf206e1b
Tito Calderon <162237728+ElPesi@users.noreply.github.com>
2024-04-12 11:29:49 -0300

    Update ej1.py

--- ej1.py
+++ ej1.py
@@ -1,1 +1,9 @@
+def intersect(list1, list2):
+  result = []
+  for element in list1:
+    if element in list2:
+      result.append(element)
+  return result
 
+
+print(intersect([1, 2, 3, 4,5], [2, 3, 5,5,6]))
